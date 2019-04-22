b2f22125ca325a98b38ad0fd1aae75271fa2c95e
Zachary Boyd <zachary@zacharyboyd.nyc>
2019-04-22 00:26:42 -0400

    Fixes small issue

--- src/build.js
+++ src/build.js
@@ -9,6 +9,8 @@ const outputFile = path.join(__dirname, '..', 'data', 'emperors.json');
 (async () => {
     const $ = await request({ url: `https://en.wikipedia.org/wiki/Holy_Roman_Emperor`, transform: cheerio.load });
 
+    $('table[cellpadding="4"]').remove();
+
     const data = flatten(
         $('.mw-parser-output h4').get().map((h4) => {
             const house = $('.mw-headline', h4).text();
@@ -18,11 +20,12 @@ const outputFile = path.join(__dirname, '..', 'data', 'emperors.json');
                 const imageUrl = `https:`+$('img', $('td', emperor)[0]).attr('src');
                 const nameElement = $(emperor).children()[$('th:contains("Name")', $(emperor).parent()).index()];
 
-                const name = $(nameElement).clone().children('small').remove().end().text().trim();
+                const name = $(nameElement).clone().children('small').remove().end().text().trim().split('[').shift();
                 const url = 'https://en.wikipedia.org'+$('a', nameElement).attr('href');
                 const reignText = $('small', nameElement).text().trim().replace(/\(/g, '').replace(/\)/g, '');
                 const reignStart = Number(reignText.split('–').shift());
                 const reignEnd = Number(reignText.split('–').pop());
+
 
                 return {
                     house,
