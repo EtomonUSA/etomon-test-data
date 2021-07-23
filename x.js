96a403d5f23d3959b3f21c3d9d89b2707841cda2
Zachary Boyd <zachary@zacharyboyd.nyc>
2021-07-23 19:46:08 -0400

    new test data, and new license

---
+++ x.js
@@ -0,0 +1,11 @@
+(async () => {
+  let { people, places } = require('./');
+  let _ = require('lodash');
+
+  console.log('person', await people[0]);
+  console.log('places', await places[0]);
+
+  // For a random one use lodash.sample or chance.shuffle
+  console.log('random person', await _.sample(people))
+  console.log('random place', await _.sample(places))
+})();
